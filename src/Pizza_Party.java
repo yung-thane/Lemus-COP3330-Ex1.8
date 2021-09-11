c1e90cd27bc264a743c12a40c27b53b4bc9da4e7
Ronald <ronald.cristobal.lemus@outlook.com>
2021-09-11 16:09:08 -0400

    Initial commit

---
+++ src/Pizza_Party.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Pizza_Party {
+    public static void main(String[] args){
+        System.out.print("How many people? ");
+        Scanner peopleInp = new Scanner(System.in);
+        int people = peopleInp.nextInt();
+
+        System.out.print("How many pizzas do you have? ");
+        Scanner pizzaInp = new Scanner(System.in);
+        int pizzas = pizzaInp.nextInt();
+
+        System.out.print("How many slices per pizza? ");
+        Scanner slicesInp = new Scanner(System.in);
+        int slices = slicesInp.nextInt();
+        int slicesTotal = slices * pizzas;
+        int slicesPer = slicesTotal / people;
+        int slicesLeft = slicesTotal % people;
+
+        System.out.printf("%d people with %d pizzas (%d slices)\n", people, pizzas, slicesTotal);
+        System.out.printf("Each person gets %d pieces of pizza.\n", slicesPer);
+        System.out.printf("There are %d leftover pieces.", slicesLeft);
+
+
+    }
+}
